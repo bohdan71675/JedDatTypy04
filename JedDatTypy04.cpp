024dc7b18730d6a618148ed3dd7b79566af7879e
bodnarb.04 <bodnarb.04@gmail.com>
2022-10-20 14:41:04 +0200

    Přidejte soubory projektu.

---
+++ JedDatTypy04.cpp
@@ -0,0 +1,26 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char x;
+    bool lichy = false;
+    cout << "Zadavej znaky zakoncena (*): " << endl;
+    cin >> x;
+
+    while (x != '*' && !lichy)
+    {
+        cout << "Znak co ho predchazi " << (char)(x - 1) << " jeho ASCII kode je " << (int)(x - 1) << endl;
+        cout << "Znak je " << x << " jeho ASCII kod je " << (int)x << endl;
+        cout << "Znak co ho predchazi " << (char)(x + 1) << " jeho ASCII kode je " << (int)(x + 1) << endl;
+
+        if ((int)x % 2 != 0)
+        {
+            lichy = true;
+        }
+        cin >> x;
+    }
+
+}
